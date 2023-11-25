939014d9bd3690b02e77600fce696ff9c1d9a8a3
dldnwlsgd <118660935+dldnwlsgd@users.noreply.github.com>
2023-11-26 01:14:31 +0900

    ㅁㄴㅇㄹ

---
+++ Assets/MRTKExtensions/QRCodes/OpenXRSpatialGraphCoordinateSystemSetter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using Microsoft.MixedReality.OpenXR;
+
+namespace MRTKExtensions.QRCodes
+{
+    public class OpenXRSpatialGraphCoordinateSystemSetter : SpatialGraphCoordinateSystemSetter
+    {
+        protected override void UpdateLocation(Guid spatialGraphNodeId, float physicalSideLength)
+        {
+            var node = spatialGraphNodeId != Guid.Empty ? SpatialGraphNode.FromStaticNodeId(spatialGraphNodeId) : null;
+            if (node != null && node.TryLocate(FrameTime.OnUpdate, out Pose pose))
+            {
+                if (Camera.main.transform.parent != null)
+                {
+                    pose = pose.GetTransformedBy(Camera.main.transform.parent);
+                }
+
+                MovePoseToCenter(pose,physicalSideLength);
+            }
+            else
+            {
+                PositionAcquisitionFailed?.Invoke(this, null);
+            }
+        }
+    }
+}
