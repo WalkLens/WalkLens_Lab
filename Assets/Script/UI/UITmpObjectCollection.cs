fda38510d1b34a7d01a41b63f3248a4edd222afd
aloHyomora <alohomoratolit@naver.com>
2023-11-25 23:06:04 +0900

    Create Canvas Student List (Scrollable)

--- Assets/Script/UI/UITmpObjectCollection.cs
+++ Assets/Script/UI/UITmpObjectCollection.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class UITmpObjectCollection : MonoBehaviour
 {
-    [SerializeField] private List<TextMeshProUGUI> tmpCollection;
+    [SerializeField] private List<GameObject> studentUIObjects;
+    [SerializeField] private List<TextMeshProUGUI> studentUITmpList;
 
+    public void LoadTMPData()
+    {
+        if (studentUIObjects == null)
+        {
+            
+        }
+    }
     public void AddStudentUI(string studentName)
     {
         
@@ -14,6 +23,16 @@ public class UITmpObjectCollection : MonoBehaviour
 
     public void SearchNameOnList(string studentName)
     {
-        
+        foreach (var tmp in studentUITmpList)
+        {
+            if (tmp.text.Contains(studentName))
+            {
+                tmp.transform.parent.gameObject.SetActive(true);
+            }
+            else
+            {
+                tmp.transform.parent.gameObject.SetActive(false);
+            }
+        }        
     }
 }
