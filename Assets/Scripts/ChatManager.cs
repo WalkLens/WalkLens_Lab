3ee660174124ef7504cf80660815427b5c8087b9
kgy6022 <kgy6022@naver.com>
2023-11-25 20:20:39 +0900

    [Revert] ChatManger.cs
    다시 이전 로직으로 되돌림

--- Assets/Scripts/ChatManager.cs
+++ Assets/Scripts/ChatManager.cs
@@ -41,18 +41,6 @@ public class ChatManager : MonoBehaviourPunCallbacks
 
     void Update()
     {
-        if (isMessageReceived)
-        {
-            GameObject goText = Instantiate(m_ActionButton, m_Content.transform);
-
-            m_ActionButton_Frontplate = goText.transform.GetChild(2).gameObject;
-            m_Frontplate_AnimatedContent = m_ActionButton_Frontplate.transform.GetChild(0).gameObject;
-            m_AnimatedContent_Text = m_Frontplate_AnimatedContent.transform.GetChild(1).gameObject;
-
-            m_AnimatedContent_Text.GetComponent<TextMeshProUGUI>().text = receivedMessage;
-            isMessageReceived = false;
-        }
-
         //if (Input.GetKeyDown(KeyCode.Return) && m_inputField.isFocused == false)
         //{
         //    m_inputField.ActivateInputField();
@@ -92,15 +80,13 @@ public class ChatManager : MonoBehaviourPunCallbacks
 
     void AddChatMessage(string message)
     {
-        receivedMessage=message;
-        isMessageReceived = true;
-        //GameObject goText = Instantiate(m_ActionButton, m_Content.transform);
+        GameObject goText = Instantiate(m_ActionButton, m_Content.transform);
 
-        //m_ActionButton_Frontplate = goText.transform.GetChild(2).gameObject;
-        //m_Frontplate_AnimatedContent = m_ActionButton_Frontplate.transform.GetChild(0).gameObject;
-        //m_AnimatedContent_Text = m_Frontplate_AnimatedContent.transform.GetChild(1).gameObject;
+        m_ActionButton_Frontplate = goText.transform.GetChild(2).gameObject;
+        m_Frontplate_AnimatedContent = m_ActionButton_Frontplate.transform.GetChild(0).gameObject;
+        m_AnimatedContent_Text = m_Frontplate_AnimatedContent.transform.GetChild(1).gameObject;
 
-        //m_AnimatedContent_Text.GetComponent<TextMeshProUGUI>().text = message;
+        m_AnimatedContent_Text.GetComponent<TextMeshProUGUI>().text = message;
 
         //goText.GetComponent<TextMeshProUGUI>().text = message;
         //m_ActionButton.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
