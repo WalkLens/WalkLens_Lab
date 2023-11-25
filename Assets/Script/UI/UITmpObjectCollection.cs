82491cbd8448fe45dfc457a4a79408530663bead
aloHyomora <alohomoratolit@naver.com>
2023-11-25 22:40:53 +0900

    Duplicate Origin Sample Scene, On Progress.. Student List Scroll bar

---
+++ Assets/Script/UI/UITmpObjectCollection.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UITmpObjectCollection : MonoBehaviour
+{
+    [SerializeField] private List<TextMeshProUGUI> tmpCollection;
+
+    public void AddStudentUI(string studentName)
+    {
+        
+    }
+
+    public void SearchNameOnList(string studentName)
+    {
+        
+    }
+}
