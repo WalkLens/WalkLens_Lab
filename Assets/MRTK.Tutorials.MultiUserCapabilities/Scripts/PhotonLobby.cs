df3d4a4f4af231b72ee6afc0a9b18fefefa657c2
kgy6022 <kgy6022@naver.com>
2023-12-17 03:30:39 +0900

    [Revert] PhotonLobby.cs

--- Assets/MRTK.Tutorials.MultiUserCapabilities/Scripts/PhotonLobby.cs
+++ Assets/MRTK.Tutorials.MultiUserCapabilities/Scripts/PhotonLobby.cs
@@ -3,26 +3,14 @@ using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using TMPro;
 
 namespace MRTK.Tutorials.MultiUserCapabilities
 {
     public class PhotonLobby : MonoBehaviourPunCallbacks
     {
         public static PhotonLobby Lobby;
-        public TextMeshProUGUI roomNameInput;
-        public TextMeshProUGUI passwordInput;
-        public GameObject student_Content;
-        public GameObject room_Content;
-        public GameObject studentBtn;
-        public GameObject roomBtn;
-
         [SerializeField]
-        private GameObject mainMenuUI;
-        [SerializeField]
-        private GameObject supervisor_connectUI;
-        [SerializeField]
-        private GameObject student_RoomListUI;
+        private GameObject identificationUI;
         [SerializeField]
         private Text stdName;
         private int roomNumber = 1;
@@ -98,46 +86,30 @@ namespace MRTK.Tutorials.MultiUserCapabilities
         }
 
         // 포톤 서버룸 생성
-        public void CreateRoom()
+        private void CreateRoom()
         {
-            string roomName = roomNameInput.text;
-            string password = passwordInput.text;
-
-            var roomOptions = new RoomOptions
-            {
-                IsVisible = true,
-                IsOpen = true,
-                MaxPlayers = 10,
-                CustomRoomProperties = new ExitGames.Client.Photon.Hashtable
-                {
-                    { "Password", password }
-                }
-            };
-            PhotonNetwork.CreateRoom(roomName, roomOptions);
-            JoinRoom();
+            var roomOptions = new RoomOptions { IsVisible = true, IsOpen = true, MaxPlayers = 10 };
+            PhotonNetwork.CreateRoom("Room" + Random.Range(1, 3000), roomOptions);
         }
 
         // Identification에서 Supervisor 선택시
         public void SetSupervisorId()
         {
-            mainMenuUI.SetActive(false);
-            supervisor_connectUI.SetActive(true); 
+            identificationUI.SetActive(false);
             PhotonNetwork.NickName = "Supervisor";
             isIdentified = true;
-            //CreateRoom();
-            //PhotonNetwork.JoinRoom(roomNameInput.text);
+            JoinRoom();
         }
 
         // Identification에서 Student 선택시
         public void SetStudentId()
         {
-            mainMenuUI.SetActive(false);
+            identificationUI.SetActive(false);
             string nickName = stdName.text;
             Debug.Log("nickName : " + nickName);
             PhotonNetwork.NickName = nickName;
             isIdentified = true;
-            student_RoomListUI.SetActive(true);
-            //PhotonNetwork.JoinRoom(roomNameInput.text);
+            JoinRoom();
         }
 
         // 포톤 서버룸 조인
@@ -146,21 +118,9 @@ namespace MRTK.Tutorials.MultiUserCapabilities
             if (isIdentified)
             {
                 Debug.Log("UI비활성화");
-                PhotonNetwork.JoinRoom(roomNameInput.text);
+                PhotonNetwork.JoinRandomRoom();
                 Debug.Log("방 진입");
             }
-        }
-        public void AddStudentBtn()
-        {
-            GameObject studentNameObject = Instantiate(studentBtn,room_Content.transform);
-            GameObject studentNameObject_Name = studentBtn.transform.GetChild(4).gameObject;
-            studentNameObject_Name.GetComponent<TextMeshProUGUI>().text = stdName.text;
-        }
-        public void AddRoomBtn()
-        {
-            GameObject roomNameObject = Instantiate(studentBtn, room_Content.transform);
-            GameObject roomNameObject_Name = studentBtn.transform.GetChild(4).gameObject;
-            roomNameObject_Name.GetComponent<TextMeshProUGUI>().text = roomNameInput.text;
         }
     }
 }
