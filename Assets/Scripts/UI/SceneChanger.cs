59fbf89aeaeaaea06328c42d783a318508610f13
kgy6022 <kgy6022@naver.com>
2024-01-14 03:03:04 +0900

    hotfix

--- Assets/Scripts/UI/SceneChanger.cs
+++ Assets/Scripts/UI/SceneChanger.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class SceneChanger : MonoBehaviour
 {
     public void ResetScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        PhotonNetwork.Disconnect();
     }
 }
